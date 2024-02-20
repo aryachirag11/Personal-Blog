44b9846a259dd0c7120a79e5e754cac20d2a9480
aryachirag11 <aryachirag11@gmail.com>
2024-02-20 21:13:00 +0530

    #2 updated : environment variables

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-  tinymceapikey: String(import.meta.env.VITE_TINYMCE_API_KEY),
+  tinymceapikey: String(import.meta.env.VITE_TINYMCE_ID),
 };
 // there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
